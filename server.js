1a4e46e5a634f66c5157c6c30a99b061fc932ca7
Mugi <mugil1504@gmail.com>
2026-04-27 14:10:52 +0530

    server update

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-const pool = require('./src/config/db');
-
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const http = require('http');
+
+const pool = require('./src/config/db');
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true
 }));
-
-app.use(express.json());
-app.use(cookieParser());
 
 const registerRoutes = require('./src/routes/register.Routes');
 const loginRoutes = require('./src/routes/login.Routes');
@@ -21,7 +22,7 @@ const otpRoutes = require('./src/routes/otp.Routes');
 const verifyOTPRoutes = require('./src/routes/verifyOTP.Routes');
 const profileRoutes = require('./src/routes/profile.Routes');
 const logoutRoutes = require('./src/routes/logout.Routes');
-const forgetpassRoutes =require('./src/routes/forgetpass.Routes')
+const forgetpassRoutes = require('./src/routes/forgetpass.Routes');
 
 app.use('/api/v1', registerRoutes);
 app.use('/api/v1', loginRoutes);
@@ -29,16 +30,22 @@ app.use('/api/v1', otpRoutes);
 app.use('/api/v1', verifyOTPRoutes);
 app.use('/api/v1', profileRoutes);
 app.use('/api/v1', logoutRoutes);
-app.use('/api/v1',forgetpassRoutes);
+app.use('/api/v1', forgetpassRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "route_not_found"
+  });
+});
 
 const { errorHandler } = require('./src/middlewares/errorMiddleware');
 app.use(errorHandler);
 
-const http = require('http');
 const server = http.createServer(app);
 
 const PORT = process.env.PORT ;
 
 server.listen(PORT, () => {
-  console.log(`Server running on PORT ${PORT}`);
+  console.log(`🚀 Server running on PORT ${PORT}`);
 });
